da30b9c1f1cddbfece7e8a55d3d5135874c86d31
devKimHD <khd30425@gmail.com>
2022-05-26 15:26:30 +0900

    commit test

--- hello3/src/ex/Hello3.java
+++ hello3/src/ex/Hello3.java
@@ -6,14 +6,14 @@ public class Hello3 {
 		System.out.println("kimHD hello a");
 		System.out.println("kimHD hello b");
 		System.out.println("수정합니다");
+		System.out.println("kimHD hello a");
+		System.out.println("kimHD hello b");
+		System.out.println("kimHD hello a");
+		System.out.println("kimHD hello b");
 		System.out.println("kimHD hello HD(Human department)");
-
-		System.out.println("KKK");
-
 		
 		int aa;
 		int bb;
 		String cc;
-
 	}
 }
